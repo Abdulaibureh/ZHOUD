7142f1874f5e8d48cd12ca0d77c69c386a4e8596
abdulaibureh01 <abdulaibureh3056@gmial.com>
2023-10-01 23:54:19 +0000

    mondat

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.lgo('Hello World');
+http = require('http')
+http.createServer( function (req, res){
+  res.writeHead(200, {'contentType' : 'text/plain'});
+  res.end('This is my career and one of my sustainable fields');
+}).listen(8081);
+console.log('server running at http://127.0.0.1:8081');
+
+
+
--- links/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-  echo"{$_POST['fname']} <br>";
-  echo"{$_POST['lname']} <br>";
-  echo"{$_POST['email']} <br>";
-  echo"{$_POST['password']} <br>";
-?>
